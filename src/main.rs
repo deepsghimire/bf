2bf6ffb52b7225e6dd8ff9083c73b21d9bfffae6
stengah <deepsghimire1@gmail.com>
2022-08-23 07:44:49 +0545

    Implemented reading from a file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,32 @@
 use bf::asm::assemble;
 use bf::machine::Machine;
 use std::process;
+use std::env;
+use std::fs;
+
+fn getfilename() -> Result<String,String> {
+    
+    let filename  = env::args().skip(1).next().ok_or(String::from("filename required"));
+    filename
+
+
+    
+}
 
 fn main() {
-    let program = ",+[-.,+]thisisnt a program";
+    
+    let filename = getfilename().unwrap_or_else(|err|{
+        eprintln!("Error command arguments: {err}");
+        process::exit(1);
+    });
+
+    let program = fs::read_to_string(&filename).unwrap_or_else(|err| {
+        eprintln!("Error reading {filename} arguments: {err}");
+        process::exit(1);
+    });
+
+
+
     let mut mach = Machine::new();
     let codes = assemble(String::from(program)).unwrap();
 
